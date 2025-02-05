c5fcd7a3bed1ddc5d60f2a2258f15198afd8c8c3
Alinia Andersson <alinia93@gmail.com>
2025-02-05 12:31:35 +0100

    hej

--- Program.cs
+++ Program.cs
@@ -111,9 +111,14 @@ builder.Services.AddScoped<AccessControl>();
 
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    // Sätt den specifika porten
+    serverOptions.ListenAnyIP(int.Parse(port));
+});
+
 var app = builder.Build();
 
-app.Run($"http://0.0.0.0:{port}");
 
 if (!app.Environment.IsDevelopment())
 {
